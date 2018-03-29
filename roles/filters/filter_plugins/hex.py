0b1f66290809262fc9396678239933c024115bc2
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-03-29 18:25:27 +0800

    Fix binascii creating bytestrings (#6)

--- roles/filters/filter_plugins/hex.py
+++ roles/filters/filter_plugins/hex.py
@@ -1,14 +1,13 @@
 import base64
-import binascii
 import os
 
 def base64_to_hex(encoded):
     """Convert base64 to hex"""
-    return binascii.hexlify(base64.b64decode(encoded))
+    return base64.b64decode(encoded).hex()
 
 def random_bytes(length):
     """Return random bytes in hex"""
-    return binascii.hexlify(os.urandom(length))
+    return os.urandom(length).hex()
 
 class FilterModule(object):
     """
