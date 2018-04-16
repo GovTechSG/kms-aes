20feceffaf82df26eb52103ccc42897309765629
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-04-16 15:42:54 +0800

    Update filter to be compatible with Python 2 (#8)
    * Update filter to be compatible with Python 2
    
    * Make flake8 happier

--- roles/filters/filter_plugins/hex.py
+++ roles/filters/filter_plugins/hex.py
@@ -1,13 +1,22 @@
 import base64
+import codecs
 import os
 
+
+# From https://stackoverflow.com/a/46878633
 def base64_to_hex(encoded):
     """Convert base64 to hex"""
-    return base64.b64decode(encoded).hex()
+    decoded = base64.b64decode(encoded)
+    b_string = codecs.encode(decoded, 'hex')
+    return b_string.decode('utf-8').upper()
+
 
 def random_bytes(length):
     """Return random bytes in hex"""
-    return os.urandom(length).hex()
+    bytes = os.urandom(length)
+    b_string = codecs.encode(bytes, 'hex')
+    return b_string.decode('utf-8').upper()
+
 
 class FilterModule(object):
     """
