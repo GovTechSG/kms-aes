d8a27648941f539777ffff0f1b3ac1a041f138cb
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-04-16 15:39:27 +0800

    Make flake8 happier

--- roles/filters/filter_plugins/hex.py
+++ roles/filters/filter_plugins/hex.py
@@ -2,19 +2,21 @@ import base64
 import codecs
 import os
 
-# From https://stackoverflow.com/a/46878633
 
+# From https://stackoverflow.com/a/46878633
 def base64_to_hex(encoded):
     """Convert base64 to hex"""
     decoded = base64.b64decode(encoded)
     b_string = codecs.encode(decoded, 'hex')
     return b_string.decode('utf-8').upper()
 
+
 def random_bytes(length):
     """Return random bytes in hex"""
     bytes = os.urandom(length)
     b_string = codecs.encode(bytes, 'hex')
     return b_string.decode('utf-8').upper()
+
 
 class FilterModule(object):
     """
